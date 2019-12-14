1a614bf528d3e0738c6844013b0d986383e9677f
Anze <info@grafolean.com>
2019-12-14 11:54:05 +0100

    SNMP bot can be started as part of grafolean services

--- snmpbot.py
+++ snmpbot.py
@@ -313,14 +313,44 @@ class SNMPBot(Collector):
             yield job_id, intervals, SNMPBot.do_snmp, job_info
 
 
+def wait_for_grafolean(backend_url):
+    while True:
+        url = '{}/status/info'.format(backend_url)
+        log.info("Checking Grafolean status...")
+        try:
+            r = requests.get(url)
+            r.raise_for_status()
+            status_info = r.json()
+            if status_info['db_migration_needed'] == False and status_info['user_exists'] == True:
+                log.info("Grafolean backend is ready.")
+                return
+        except:
+            pass
+        log.info("Grafolean backend not available / initialized yet, waiting.")
+        time.sleep(10)
+
+
 if __name__ == "__main__":
     dotenv.load_dotenv()
 
     backend_url = os.environ.get('BACKEND_URL')
-    bot_token = os.environ.get('BOT_TOKEN')
-    if not backend_url or not bot_token:
-        raise Exception("Please specify BACKEND_URL and BOT_TOKEN env vars.")
     jobs_refresh_interval = int(os.environ.get('JOBS_REFRESH_INTERVAL', 120))
+
+    if not backend_url:
+        raise Exception("Please specify BACKEND_URL and BOT_TOKEN / BOT_TOKEN_FROM_FILE env vars.")
+
+    wait_for_grafolean(backend_url)
+
+    bot_token = os.environ.get('BOT_TOKEN')
+    if not bot_token:
+        # bot token can also be specified via contents of a file:
+        bot_token_from_file = os.environ.get('BOT_TOKEN_FROM_FILE')
+        if bot_token_from_file:
+            with open(bot_token_from_file, 'rt') as f:
+                bot_token = f.read()
+
+    if not bot_token:
+        raise Exception("Please specify BOT_TOKEN / BOT_TOKEN_FROM_FILE env var.")
 
     c = SNMPBot(backend_url, bot_token, jobs_refresh_interval)
     c.execute()
